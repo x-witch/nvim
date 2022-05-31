62cfc879af0073b4cc46ba3ba685c74fc79b8116
xssaw <2815657324@qq.com>
2022-05-31 19:58:50 +0800

    修改配置

--- init.lua
+++ init.lua
@@ -1,6 +1,3 @@
--- Author: aksfiy
--- Applicable platform: Linux (tested)
--- Github: https://github.com/askfiy/nvim
 -- 加载缓存优化 ./cache/nvim/luacache_chunks文件
 --require('impatient') -- This needs to be first
 
